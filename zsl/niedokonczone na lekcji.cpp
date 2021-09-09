faf9cd4d04b22252fbc57cb2fa940b3aaab05834
Romanek <89982697+RomanGumeniuk@users.noreply.github.com>
2021-09-09 16:41:01 +0200

    Add files via upload

---
+++ zsl/niedokonczone na lekcji.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+class rectangle {
+float a;
+float b;
+
+void circuit()	
+ float c = (2*a) + (2*b);
+ cout << "Obwód tego prostok¹ta to: " << c;
+};
+
+int main(int argc, char** argv) {
+	setlocale(LC_CTYPE, "Polish");
+	rectangle.siurname(){
+		
+	}
+	
+	return 0;
+}
